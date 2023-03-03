bbe1e582a72c948963b20c731d1d1467447f5241
Frank <https://github.com/Frankmsanchez/Python.git>
2023-03-03 11:50:26 -0600

    New changes

---
+++ funcionesCreadas.py
@@ -0,0 +1,16 @@
+import operacionesBasicas.Calculadora as Calc
+import pprint
+
+
+resTsuma= Calc.suma(2,4)
+print(resTsuma)
+
+resTresta = Calc.resta(10,3)
+print(resTresta)
+
+
+resTmulti= Calc.multiplicacion(2,5)
+print(resTmulti)
+
+resTdivi= Calc.division(10,2)
+print(resTdivi)
